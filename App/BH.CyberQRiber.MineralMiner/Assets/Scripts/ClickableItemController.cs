357927b7a5a354ff2d1e61063af62841a4380b5b
Darko Kotoski <wbountyhunter@hotmail.com>
2018-06-30 15:28:09 +0200

    Added Clickable Item Prefab
    Added Clickable Item Prefab

---
+++ App/BH.CyberQRiber.MineralMiner/Assets/Scripts/ClickableItemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickableItemController : MonoBehaviour
+{
+    private DateTime LastClicked;
+    private TimeSpan RefreshTime;
+    private DateTime PlannedRefresh;
+
+    public int RefreshTimeSeconds;
+
+    public Slider Slider { get; set; }
+
+    public bool CanBeClicked { get { return SliderValue() <= 0; } }
+
+
+    void Start()
+    {
+        Slider = GetComponent<Slider>();
+        RefreshTime = new TimeSpan(RefreshTimeSeconds*10000000);
+        Click();
+        Slider.value = SliderValue();
+
+    }
+
+    void Update()
+    {
+        Slider.value = SliderValue();
+    }
+
+    float SliderValue()
+    {
+
+
+        if (LastClicked != null && PlannedRefresh != null)
+        { 
+            var res = (PlannedRefresh.Ticks - DateTime.Now.Ticks)/ (float)RefreshTime.Ticks;
+            if (res < 0) return 0;
+            return res;
+        }
+        return 0;
+    }
+
+    public void Click()
+    {
+        LastClicked = DateTime.Now;
+        PlannedRefresh = LastClicked + RefreshTime;
+    }
+}
